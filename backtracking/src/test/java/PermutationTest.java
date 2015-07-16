d570d4a64a63a0140b6471d9734abd947b740821
yaningxu <yaning0123@gmail.com>
2015-07-16 20:47:33 +0800

    implement permutate recursively

---
+++ backtracking/src/test/java/PermutationTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class PermutationTest {
+    @Test
+    public void permutateTest() {
+        System.out.println(Permutation.recPermutate("abcde").size());
+    }
+}
