9be31e94f29dcd60f0bcc41f4ae9ec2f4d4d67ae
yaningxu <yaning0123@gmail.com>
2015-07-18 10:29:09 +0800

    implement subsetsum using backtracking recursively

--- backtracking/src/main/java/SubSetSum.java
+++ backtracking/src/main/java/SubSetSum.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Backtracking Algorithm for Subset Sum
  *
@@ -8,4 +12,50 @@
  */
 public class SubSetSum {
 
+    public static List<List<Integer>> recSubsetSum(int[] array, int sum) {
+        Arrays.sort(array);
+        List<Integer> intList = new LinkedList<Integer>();
+        for (int i = 0; i < array.length; i++) {
+            intList.add(array[i]);
+        }
+        return doRecSubsetSum(intList, sum);
+    }
+
+    private static List<List<Integer>> doRecSubsetSum(List<Integer> intList, int sum) {
+
+        List<List<Integer>> resultList = new LinkedList<List<Integer>>();
+        for (int i = 0; i < intList.size(); i++) {
+            if (intList.get(i) == sum) { // base case
+                List<Integer> innerList = new LinkedList<Integer>();
+                innerList.add(sum);
+                resultList.add(innerList);
+            } else if (intList.get(i) < sum) {
+                List<List<Integer>> nextList =
+                        doRecSubsetSum(intList.subList(i + 1, intList.size()), sum - intList.get(i));
+                for (int j = 0; j < nextList.size(); j++) {
+                    List<Integer> innerList = new LinkedList<Integer>();
+                    innerList.add(intList.get(i));
+                    innerList.addAll(nextList.get(j));
+                    resultList.add(innerList);
+                }
+            } else {
+                break;
+            }
+        }
+
+        return resultList;
+    }
+
+    public static List<List<Integer>> iterSubsetSum(int[] array, int sum) {
+        Arrays.sort(array);
+        List<Integer> intList = new LinkedList<Integer>();
+        for (int i = 0; i < array.length; i++) {
+            intList.add(array[i]);
+        }
+        return doIterSubsetSum(intList, sum);
+    }
+
+    private static List<List<Integer>> doIterSubsetSum(List<Integer> intList, int sum) {
+        return null;
+    }
 }
--- backtracking/src/test/java/SubSetSumTest.java
+++ backtracking/src/test/java/SubSetSumTest.java
@@ -1,3 +1,22 @@
-public class SubSetSumTest {
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubSetSumTest {
+    @Test
+    public void subsetSumTest() {
+        int[] ints = new int[] {2, 4, 1, 6, 5};
+        List<List<Integer>> resultList = SubSetSum.recSubsetSum(ints, 15);
+        System.out.println(resultList);
+    }
+
+    @Test
+    public void testList() {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.subList(3, list.size()));
+    }
 }
