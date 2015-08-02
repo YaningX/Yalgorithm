2886b68c71c7eb4866c3cdc918081084d7d78d52
YaningX <yaning0123@gmail.com>
2015-08-02 18:50:51 +0800

    refine code

---
+++ data-structure/list/src/main/java/com/github/yaningx/yalgorithm/SinglyLinkedList.java
@@ -0,0 +1,14 @@
+package com.github.yaningx.yalgorithm;
+
+public class SinglyLinkedList<T> {
+    private Node<T> start;
+    private Node<T> end;
+
+    public SinglyLinkedList() {
+        this.start = null;
+
+    }
+    public SinglyLinkedList reverse() {
+        return null;
+    }
+}
