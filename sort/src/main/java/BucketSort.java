4e3b1c2bedce02ee98d546688640251f0be73991
yaningxu <yaning0123@gmail.com>
2015-08-06 21:07:11 +0800

    add bucket sort framework

--- sort/src/main/java/BucketSort.java
+++ sort/src/main/java/BucketSort.java
@@ -1,3 +1,12 @@
+/**
+ * 1) Time complexity O(n)
+ * 2) can be stable
+ * 3) Memory cost, complexity:
+ * Useful for: when input is uniformly distributed over a range, such as
+ *          Sort a large set of floating point numbers which are in range
+ *          from 0.0 to 1.0 and are uniformly distributed across the range.
+ *          How do we sort the numbers efficiently?
+ */
 public class BucketSort implements Sort {
     @Override
     public int[] sort(int[] numbers) {
