e90ba250dedf53ff30665a43e88c8c3948f42840
YaningX <yaning0123@gmail.com>
2015-11-28 18:01:59 +0800

    N-Queens will be solved

--- backtracking/src/main/java/NQueens.java
+++ backtracking/src/main/java/NQueens.java
@@ -1,6 +1,12 @@
+import java.util.List;
+
 /**
  * leetcode 51. https://leetcode.com/problems/n-queens/
  */
 public class NQueens {
-    
+    public List<List<String>> recSolveNQueens(int n) {
+    }
+
+    public List<List<String>> iteSolveNQueens(int n) {
+    }
 }
