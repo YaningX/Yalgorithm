3184baca7b84b59befd6d22507333b1bd55e8c5c
YaningX <yaning0123@gmail.com>
2015-12-29 15:23:26 +0800

    solve leetcode 53

---
+++ leetcode/src/main/java/dp/Leet53.java
@@ -0,0 +1,20 @@
+package dp;
+
+/**
+ * Created by xuyaning on 29/12/15.
+ */
+public class Leet53 {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int result = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            result = Math.max(dp[i], result);
+        }
+        return result;
+    }
+}
