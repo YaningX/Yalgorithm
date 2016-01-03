a83e69cc8b3fcb554bc70cdc885fe12eef6c7898
YaningX <yaning0123@gmail.com>
2016-01-03 11:15:31 +0800

    solve leet 66

---
+++ leetcode/src/main/java/dp/Leet65.java
@@ -0,0 +1,9 @@
+package dp;
+
+/**
+ * Created by xuyaning on 3/1/16.
+ */
+public class Leet65 {
+    //有限状态机
+
+}
---
+++ leetcode/src/main/java/math/Leet66.java
@@ -0,0 +1,23 @@
+package math;
+
+/**
+ * Created by xuyaning on 3/1/16.
+ */
+public class Leet66 {
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int sum = digits[i] + carry;
+            carry = sum / 10;
+            digits[i] = sum % 10;
+        }
+        if (carry == 0) {
+            return digits;
+        } else {
+            int[] result = new int[digits.length + 1];
+            result[0] = carry;
+            System.arraycopy(digits, 0, result, 1, digits.length);
+            return result;
+        }
+    }
+}
