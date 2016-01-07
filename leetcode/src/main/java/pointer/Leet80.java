7ccdc342fe0bc54e0f2e5d44334a20d456c4f0b3
YaningX <yaning0123@gmail.com>
2016-01-07 21:54:56 +0800

    solve leet 80

---
+++ leetcode/src/main/java/pointer/Leet80.java
@@ -0,0 +1,29 @@
+package pointer;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by xuyaning on 7/1/16.
+ */
+public class Leet80 {
+    public int removeDuplicates(int[] nums) {
+        Map<Integer, Integer> counterMap = new TreeMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (counterMap.containsKey(nums[i])) {
+                counterMap.put(nums[i], counterMap.get(nums[i]) + 1);
+            } else {
+                counterMap.put(nums[i], 1);
+            }
+        }
+
+        int i = 0;
+        for (Map.Entry<Integer, Integer> entry: counterMap.entrySet()) {
+            nums[i++] = entry.getKey();
+            if (entry.getValue() >= 2) {
+                nums[i++] = entry.getKey();
+            }
+        }
+        return i;
+    }
+}
