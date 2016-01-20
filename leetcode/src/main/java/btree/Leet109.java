368d8a0f78fb89241753513f97e1e9df8a456213
YaningX <yaning0123@gmail.com>
2016-01-20 10:06:55 +0800

    solve leet 109

--- leetcode/src/main/java/btree/Leet109.java
+++ leetcode/src/main/java/btree/Leet109.java
@@ -1,7 +1,37 @@
 package btree;
 
+import linkedlist.ListNode;
+
 /**
  * http://bangbingsyb.blogspot.com/2014/11/leetcode-convert-sorted-list-to-binary.html
  */
 public class Leet109 {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        int len = 0;
+        ListNode p = head;
+        while (p != null) {
+            p = p.next;
+            len++;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        return dfs(dummy, 0, len - 1);
+    }
+
+    private TreeNode dfs(ListNode dummy, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = start + (end - start) / 2;
+        TreeNode left = dfs(dummy, start, mid - 1);
+        TreeNode parent = new TreeNode(dummy.next.val);
+        parent.left = left;
+        dummy.next = dummy.next.next;
+        parent.right = dfs(dummy, mid + 1, end);
+        return parent;
+    }
 }
---
+++ leetcode/src/main/java/linkedlist/ListNode.java
@@ -0,0 +1,12 @@
+package linkedlist;
+
+/**
+ * Created by xuyaning on 20/1/16.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode (int x) {
+        val = x;
+    }
+}
