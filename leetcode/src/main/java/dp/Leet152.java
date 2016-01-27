24f5ce83cc7b9c006276d7f00dc9743a4149aa7e
YaningX <yaning0123@gmail.com>
2016-01-27 13:37:12 +0800

    solve leet 152

---
+++ leetcode/src/main/java/dp/Leet152.java
@@ -0,0 +1,22 @@
+package dp;
+
+/**
+ * Created by xuyaning on 27/1/16.
+ */
+public class Leet152 {
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = nums[0];
+        int localMax = nums[0];
+        int localMin = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int localMaxCopy = localMax;
+            localMax = Math.max(Math.max(nums[i], nums[i] * localMax), nums[i] * localMin);
+            localMin = Math.min(Math.min(nums[i], nums[i] * localMaxCopy), nums[i] * localMin);
+            max = Math.max(max, localMax);
+        }
+        return max;
+    }
+}
