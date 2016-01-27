095426de8a5c35f24e065efed0958b6914073b54
YaningX <yaning0123@gmail.com>
2016-01-27 17:58:40 +0800

    solve leet 153

---
+++ leetcode/src/main/java/bsearch/Leet153.java
@@ -0,0 +1,22 @@
+package bsearch;
+
+/**
+ * Created by xuyaning on 27/1/16.
+ */
+public class Leet153 {
+    public int findMin(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int left = 0, right = nums.length - 1;
+        while (nums[left] > nums[right]) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
