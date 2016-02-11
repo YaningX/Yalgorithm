d556b5fb7434b9682640102c30ac5719b884e5a1
YaningX <yaning0123@gmail.com>
2016-02-11 10:48:19 +0800

    solve leet 240

---
+++ leetcode/src/main/java/math/Leet240.java
@@ -0,0 +1,24 @@
+package math;
+
+/**
+ * Created by xuyaning on 11/2/16.
+ */
+public class Leet240 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
