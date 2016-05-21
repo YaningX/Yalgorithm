9f2cd57a7f091bb0ea9bb8310c04a0fba8d255d1
YaningX <yaning0123@gmail.com>
2016-05-21 11:07:55 +0800

    implement a binary indexed tree

--- leetcode/src/main/java/BitTree.java
+++ leetcode/src/main/java/BitTree.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -6,7 +7,15 @@ import java.util.List;
  * http://www.cnblogs.com/xudong-bupt/p/3484080.html
  */
 public class BitTree {
-    private List<Integer> tree = new ArrayList<Integer>();
+    public List<Integer> tree = new ArrayList<Integer>();
+
+    public BitTree() {}
+
+    public BitTree(int[] nums) {
+        for (int num: nums) {
+            this.add(num);
+        }
+    }
 
     public void add(int num) {
         int size = tree.size();
@@ -20,17 +29,65 @@ public class BitTree {
             num += tree.get(pre);
             pre = pre - ((pre ^ (pre + 1)) & (pre + 1));
         }
+        tree.add(num);
     }
 
     public void update(int k, int num) {
+        int old = this.getElement(k);
+        int diff = num - old;
+        while (k < tree.size()) {
+            tree.set(k, tree.get(k) + diff);
+            int lowbit = (k ^ (k + 1)) & (k + 1);
+            k += lowbit;
+        }
+    }
 
+    public int getElement(int k) {
+        int num = tree.get(k);
+        if ((k & 1) == 0) {
+            return num;
+        }
+        int pre = k - 1;
+        int lowbit = (pre ^ (pre + 1)) & (pre + 1);
+        while (pre >=0 & (pre + lowbit) == k) {
+            num -= tree.get(pre);
+            pre -= lowbit;
+            lowbit = (pre ^ (pre + 1)) & (pre + 1);
+        }
+        return num;
     }
 
     public int getSum(int k) {
-        return 0;
+        int sum = 0;
+        while (k >= 0) {
+            sum += tree.get(k);
+            int lowbit = (k ^ (k + 1)) & (k + 1);
+            k -= lowbit;
+        }
+        return sum;
     }
 
     public static int[] toBitTree(int[] nums) {
-        return new int[1];
+        int[] tree = new int[nums.length];
+        System.arraycopy(nums, 0, tree, 0, nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            int lowbit = (i ^ (i + 1)) & (i + 1);
+            int father = i + lowbit;
+            if (father < nums.length) {
+                tree[father] += tree[i];
+            }
+        }
+        return tree;
+    }
+
+    public static void main(String[] argvs) {
+        int[] nums = new int[] {1, 2, 3, -1, 4, 5, 6};
+        int[] bitTree = BitTree.toBitTree(nums);
+        System.out.println(Arrays.toString(bitTree));
+        System.out.println(BinaryIndexedTreeUtils.getSum(bitTree, 4));
+
+        BitTree binaryIndexTree = new BitTree(nums);
+        System.out.println(binaryIndexTree.tree);
+
     }
 }
--- leetcode/src/main/java/RedBlackBST.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by xuyaning on 27/4/16.
- */
-public class RedBlackBST {
-}
